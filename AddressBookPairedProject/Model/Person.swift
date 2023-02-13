4ca57e8cc2ce457693f1c9e7e33e04b4bd7c545a
Matthew Hill <matthill265@gmail.com>
2023-02-13 14:46:57 -0700

    Person Model was made

---
+++ AddressBookPairedProject/Model/Person.swift
@@ -0,0 +1,29 @@
+//
+//  Person.swift
+//  AddressBookPairedProject
+//
+//  Created by Matthew Hill on 2/13/23.
+//
+
+import Foundation
+
+class Person {
+    let id: UUID
+    var name: String
+    var address: String
+    
+    init(id: UUID = UUID(), name: String, address: String) {
+        self.id = id
+        self.name = name
+        self.address = address
+    }
+    
+} //End of class
+
+extension Person: Equatable {
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    
+}
