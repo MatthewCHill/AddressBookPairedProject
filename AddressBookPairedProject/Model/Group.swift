eb8d691f177e0964050063e71c72feed9110ee41
Matthew Hill <matthill265@gmail.com>
2023-02-13 14:55:27 -0700

    Group Model was made

--- AddressBookPairedProject/Model/Group.swift
+++ AddressBookPairedProject/Model/Group.swift
@@ -1,5 +1,5 @@
 //
-//  Person.swift
+//  Group.swift
 //  AddressBookPairedProject
 //
 //  Created by Matthew Hill on 2/13/23.
@@ -7,21 +7,22 @@
 
 import Foundation
 
-class Person {
+class Group {
     let id: UUID
     var name: String
-    var address: String
+    var people: [Person]
     
-    init(id: UUID = UUID(), name: String, address: String) {
+    init(id: UUID = UUID(), name: String, people: [Person] = []) {
         self.id = id
         self.name = name
-        self.address = address
+        self.people = people
     }
-    
 } //End of class
 
-extension Person: Equatable {
-    static func == (lhs: Person, rhs: Person) -> Bool {
+extension Group: Equatable {
+    static func == (lhs: Group, rhs: Group) -> Bool {
         lhs.id == rhs.id
     }
+    
+    
 }
