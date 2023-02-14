581d567e3e904064f8b597c1ca3e725719016b04
Matthew Hill <matthill265@gmail.com>
2023-02-14 15:00:14 -0700

    Updated my Story Board

---
+++ AddressBookPairedProject/ViewControllers/CustomView/PersonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PersonTableViewCell.swift
+//  AddressBookPairedProject
+//
+//  Created by Matthew Hill on 2/14/23.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
